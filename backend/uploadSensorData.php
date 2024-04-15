ca21c0454c75e49e2883a334039127e39b4145b2
Eva Baranyi <ebaranyi@lenovo.com>
2024-04-15 17:33:10 +0200

    modify upload

--- backend/uploadSensorData.php
+++ backend/uploadSensorData.php
@@ -4,13 +4,13 @@
 
     include ('./init.php');
     include ('./functions.php');
-
+    print($_POST['sensor']);
     $sensor = json_decode($_POST['sensor'], true);
 
     $sql = "INSERT INTO sd_watering (soil1, soil2, soil3, soil4, relay1, relay2, relay3, relay4)
         VALUES (".$sensor['soil1'].", ".$sensor['soil2'].", ".$sensor['soil3'].", ".$sensor['soil4'].", ".$sensor['relay1'].", ".$sensor['relay2'].", ".$sensor['relay3'].", ".$sensor['relay4'].");";
 
     $r = mysqli_query($connect, $sql);
-
-    print ($r);
+    print($r);
+    print($sql);
 ?>
